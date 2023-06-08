4c51aa7e07d55e35541c1e25ee1e47be704a3960
Andrii Gadar <113310900+UserAndrii@users.noreply.github.com>
2023-06-08 12:56:40 +0000

    fixed index.js

--- src/index.js
+++ src/index.js
@@ -7,9 +7,9 @@ import App from 'components/App';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  // <React.StrictMode>
+   <React.StrictMode>
   <Provider store={store}>
     <App />
   </Provider>
-  // </React.StrictMode>
+   </React.StrictMode>
 );
